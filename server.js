4deb93f373c51a02e1de37e8e9a7ef7edcc671d5
victor <victor.bv.996@outlook.com>
2021-04-11 20:31:51 -0400

    'asfasf'

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-// // server.js
-// const express = require('express');
-// const compression = require('compression');
-// const path = require('path');
-// const app = express();
+// server.js
+const express = require('express');
+const compression = require('compression');
+const path = require('path');
+const app = express();
 
-// app.use(compression());
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('*', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, () => {
-//     console.log(`App is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+});
